9d80199405a8c62f42c15395ddcff8946ffecf50
Namrata Nikam <63784185+Namrata-nikam@users.noreply.github.com>
2023-07-03 16:50:02 +0530

    frontend file created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./Navbar";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+
+  const handleDeploy = () => {
+    console.log("name:", name, "symbol:", symbol);
+
+    axios
+      .post("http://localhost:4000/api/contractdata", {
+        name: name,
+        symbol: name,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleVerify = () => {
+    console.log("name:", name, "symbol:", symbol);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="card">
+        <div>
+          <div>
+            <label className="label">Name</label>
+          </div>
+          <div>
+            <input
+              className="inputText"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter name"
+            />
+          </div>
+        </div>
+        <div className="p-input-icon-left">
+          <div>
+            <label className="label">Symbol</label>
+          </div>
+          <div>
+            <input
+              className="inputText"
+              type="text"
+              onChange={(e) => setSymbol(e.target.value)}
+              placeholder="Enter symbol"
+            />
+          </div>
+        </div>
+        <button className="btn" onClick={() => handleDeploy()}>
+          Deploy
+        </button>
+        <button className="btn" onClick={() => handleVerify()}>
+          Verify
+        </button>
+      </div>
+    </>
   );
 }
 
